c37cba124a5433a1544047576faea54b968b0171
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-09-04 10:08:55 +0300

    Design tableViewCell and implement its class

---
+++ Milestone-Projects 10-12/TableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.swift
+//  Milestone-Projects 10-12
+//
+//  Created by othman shahrouri on 9/4/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet var userImage: UIImageView!
+    @IBOutlet var caption: UILabel!
+    
+   
+
+}
